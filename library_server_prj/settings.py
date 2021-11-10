4de057dfd09f94031e415f7a37938e5820c2b5b0
DESKTOP-5GA4I66\hyosung_daily <gon_1107@naver.com>
2021-11-10 17:23:45 +0900

    아두이노 와이파이 api  기능 테스트용 코드 추가

--- library_server_prj/settings.py
+++ library_server_prj/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'book',
     'single_pages',
+    'api',
 ]
 
 MIDDLEWARE = [
--- library_server_prj/urls.py
+++ library_server_prj/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api/', include('api.urls')),
     path('book/', include('book.urls')),
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
